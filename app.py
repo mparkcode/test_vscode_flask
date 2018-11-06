8f5e0f9bae37b5bdc8cb209431b6601124accfe9
Michael Park <michaelpark@Michaels-MacBook-Pro.local>
2018-11-06 20:48:20 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/testing")
+def testing():
+    return "Testing"
+
+if __name__ == "__main__":
+    app.run(debug=True)   
