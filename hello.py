1bef297bf1e3de74ec28139b73f75aee85e59d91
Michael Park <michaelpark@Michaels-MacBook-Pro.local>
2018-11-07 14:32:31 +0000

    updated hello.py

--- hello.py
+++ hello.py
@@ -10,5 +10,5 @@ def hello():
 def testing():
     return "Testing"
 
-if __name__ == "__main__":
-    app.run()   
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=False) 
